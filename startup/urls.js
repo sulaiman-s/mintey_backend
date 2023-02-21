7ce3ead38f039dfd31384e3fa65e3062c8f1b1a6
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-02-21 23:52:28 +0500

    update

--- startup/urls.js
+++ startup/urls.js
@@ -1,8 +1,10 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors=require('cors');
 const authRouter = require("../routes/auth");
 
 module.exports = function (app) {
+  app.use(cors({origin:"*"}));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(morgan("tiny"));
